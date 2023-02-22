34146793ef619224df430a8cca28eb9878107cb8
Kavindu Fernando <kavindu2331@gmail.com>
2023-02-22 10:20:02 +0530

    fix: changed to class-based views from functional views

--- apps/assesment/urls.py
+++ apps/assesment/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from .views import avalability_list, avalability_detail
+from rest_framework.urlpatterns import format_suffix_patterns
 from apps.assesment import views
 
 urlpatterns = [
-    path('avalability/', views.avalability_list),
-    path('avalability/<int:pk>/', views.avalability_detail),
+    path('avalability/', views.avalability_list.as_view()),
+    path('avalability/<int:pk>/', views.avalability_detail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- apps/assesment/views.py
+++ apps/assesment/views.py
@@ -1,53 +1,57 @@
 from django.shortcuts import render
-from rest_framework import viewsets
 from .models import Avalability
 from .serializers import AvalabilitySerializer
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+# Create your views here.
 
 
-@csrf_exempt
-def avalability_list(request):
+class avalability_list(APIView):
     """
     List all avalability, or create a new avalability.
     """
-    if request.method == 'GET':
+
+    def get(self, request, format=None):
         avalability = Avalability.objects.all()
         serializer = AvalabilitySerializer(avalability, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = AvalabilitySerializer(data=data)
+    def post(self, request, format=None):
+        serializer = AvalabilitySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def avalability_detail(request, pk):
+class avalability_detail(APIView):
     """
-    Retrieve, update or delete a avalability.
+    Retrieve, update or delete a avalability instance.
     """
-    try:
-        avalability = Avalability.objects.get(pk=pk)
-    except Avalability.DoesNotExist:
-        return HttpResponse(status=404)
 
-    if request.method == 'GET':
+    def get_object(self, pk):
+        try:
+            return Avalability.objects.get(pk=pk)
+        except Avalability.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        avalability = self.get_object(pk)
         serializer = AvalabilitySerializer(avalability)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = AvalabilitySerializer(avalability, data=data)
+    def put(self, request, pk, format=None):
+        avalability = self.get_object(pk)
+        serializer = AvalabilitySerializer(avalability, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        avalability = self.get_object(pk)
         avalability.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
